29ee6f4c663b7c040a67cc424e81bc2c9fc204c0
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-02-07 16:17:13 +0000

    setup_shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
    return (
       <div>
-         <h1>Home</h1>
+         <h1 className="text-white text-3xl underline">Home</h1>
+         <button>Click</button>
       </div>
    );
 }
