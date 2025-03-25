e5707764c26f8793b1f79cce26c66851ddb83ba7
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-03-25 15:32:54 +0000

    deploy-format

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 import { withSentryConfig } from '@sentry/nextjs';
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-   /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
    typescript: {
       ignoreBuildErrors: true,
    },
