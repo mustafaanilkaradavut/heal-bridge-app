3f94b0ad4d1a79af5d4f7cd584f822ba6cc1e96d
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-03-25 15:44:09 +0000

    deploy

--- next.config.js
+++ next.config.js
@@ -1,20 +1,13 @@
-import { withSentryConfig } from '@sentry/nextjs';
+const { withSentryConfig } = require('@sentry/nextjs');
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-   typescript: {
-      ignoreBuildErrors: true,
-   },
-   eslint: {
-      ignoreDuringBuilds: true,
-   },
-};
+const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
    // For all available options, see:
-   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-   org: 'anil-karadavut',
-   project: 'javascript-nextjs',
+   org: 'javascript-mastery',
+   project: 'care-pulse',
 
    // Only print logs for uploading source maps in CI
    silent: !process.env.CI,
@@ -25,11 +18,14 @@ export default withSentryConfig(nextConfig, {
    // Upload a larger set of source maps for prettier stack traces (increases build time)
    widenClientFileUpload: true,
 
-   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
    // This can increase your server load as well as your hosting bill.
    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
    // side errors will fail.
-   tunnelRoute: '/monitoring',
+   // tunnelRoute: "/monitoring",
+
+   // Hides source maps from generated client bundles
+   hideSourceMaps: true,
 
    // Automatically tree-shake Sentry logger statements to reduce bundle size
    disableLogger: true,
